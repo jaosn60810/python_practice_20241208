4b9ee71045b59b078745fd7c9af8c9e8d20683cc
jason60810 <26405746+jaosn60810@users.noreply.github.com>
2024-12-31 03:16:27 +0000

    Update order status and refactor solutions for problems 3 and 4. Marked additional problems as completed in order.md. Refactored solution.py for problem 3 to improve clarity and efficiency, and enhanced solution.py for problem 4 by optimizing slope calculation for collinear points.

--- problem_3/solution.py
+++ problem_3/solution.py
@@ -1,10 +1,41 @@
 # 第3題: 雞蛋大盜
 n = int(input())
 eggs = list(map(int, input().split()))
-dp = [0] * (n + 1)
-dp[1] = eggs[0]
+# dp = [0] * (n + 1)
+# dp[1] = eggs[0]
 
-for i in range(2, n + 1):
-    dp[i] = max(dp[i - 1], dp[i - 2] + eggs[i - 1])
+# for i in range(2, n + 1):
+#     dp[i] = max(dp[i - 1], dp[i - 2] + eggs[i - 1])
 
-print(dp[n]) 
+# print(dp[n]) 
+
+
+rob1, rob2 = 0, 0
+
+# [rob1, rob2, n, n+1, ...]
+for n in eggs:
+    tmp = max(rob1 + n, rob2)
+    rob1 = rob2
+    rob2 = tmp
+print(rob2)
+
+# [1,2,3,1]
+# n = 1
+# tmp = max(rob1 + n, rob2) = max(0 + 1, 0) = 1
+# rob1 = rob2 = 0
+# rob2 = tmp = 1
+
+# n = 2
+# tmp = max(rob1 + n, rob2) = max(0 + 2, 1) = 2
+# rob1 = rob2 = 1
+# rob2 = tmp = 2
+
+# n = 3
+# tmp = max(rob1 + n, rob2) = max(1 + 3, 2) = 4
+# rob1 = rob2 = 2
+# rob2 = tmp = 4
+
+# n = 1
+# tmp = max(rob1 + n, rob2) = max(2 + 1, 4) = 4
+# rob1 = rob2 = 4
+# rob2 = tmp = 4
--- problem_4/solution.py
+++ problem_4/solution.py
@@ -8,15 +8,15 @@ points = list(set(points))  # 去除重複點
 # Calculate the maximum number of collinear points
 max_points = 1  # 初始化為1，因為至少有一個點
 for i in range(len(points)):
+    point1 = points[i]
     slopes = defaultdict(int)
     for j in range(i + 1, len(points)):  # 只需要檢查i之後的點
-        dx = points[j][0] - points[i][0]
-        dy = points[j][1] - points[i][1]
+        points2 = points[j]
         
-        if dx == 0:  # 垂直線的情況
+        if points2[0] == point1[0]:  # 垂直線的情況
             slope = float('inf')
         else:
-            slope = dy / dx  # 使用浮點數斜率
+            slope = (points2[1] - point1[1]) / (points2[0] - point1[0]) # 使用浮點數斜率
         slopes[slope] += 1
     
         max_points = max(max_points, max(slopes.values()) + 1)  # +1 為當前點本身
